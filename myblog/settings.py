1f7868d0eb4ce5032f102911641e577d75940897
Stefan <stefanbushgordon@gmail.com>
2018-08-12 10:26:59 +0100

    Added HTML and CSS to site, view and template for blog posts

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = sggss
 
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
